567439dbfa6fc02438bdb4e054306198200c3d49
astralvoid08-code <astralvoid08@gmail.com>
2026-07-19 17:00:55 +0530

    Update can.rs

--- drivers/src/bus/can.rs
+++ drivers/src/bus/can.rs
@@ -1,10 +1,18 @@
+#[derive(Debug, Clone, Copy)]
 pub struct CanFrame {
     pub id: u32,
     pub dlc: u8,
     pub data: [u8; 8],
 }
 
+#[derive(Debug)]
+pub enum CanError {
+    BusOff,
+    Overrun,
+    InvalidDlc,
+}
+
 pub trait CanBus {
-    fn send(&mut self, f: CanFrame) -> Result<(), ()>;
-    fn recv(&mut self) -> Option<CanFrame>;
+    fn send(&mut self, frame: &CanFrame) -> Result<(), CanError>;
+    fn recv(&mut self) -> Result<Option<CanFrame>, CanError>;
 }
